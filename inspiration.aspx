53c9b033d79b1f8113e02b1235403b9e854501b5
Derek Sellers <dseller1@stumail.jccc.edu>
2015-11-05 18:46:53 -0600

    Inspiration Page

--- inspiration.aspx
+++ inspiration.aspx
@@ -1,7 +1,26 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="inspiration.aspx.cs" MasterPageFile="MainMaster.master" Inherits="inspiration" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <asp:Image ID="colorPlace1" runat="server" CssClass="colorSwatch" />
+    <asp:Image ID="colorPlace2" runat="server" CssClass="colorSwatch" />
+    <asp:Image ID="colorPlace3" runat="server" CssClass="colorSwatch" />
+    <asp:Image ID="chairPlace1" runat="server" CssClass="chairSwatch" />
+    <asp:Image ID="chairPlace2" runat="server" CssClass="chairSwatch" />
+    <asp:Image ID="chairPlace3" runat="server" CssClass="chairSwatch" />
+    <asp:Image ID="rugPlace1" runat="server" CssClass="rugSwatch" />
+    <asp:Image ID="rugPlace2" runat="server" CssClass="rugSwatch" />
+    <asp:Image ID="rugPlace3" runat="server" CssClass="rugSwatch" />
+    <asp:Image ID="patternPlace1" runat="server" CssClass="patternSwatch" />
+    <asp:Image ID="patternPlace2" runat="server" CssClass="patternSwatch" />
+    <asp:Image ID="patternPlace3" runat="server" CssClass="patternSwatch" />
+    <asp:Image ID="roomPlace1" runat="server" CssClass="roomSwatch" />
+    <asp:Image ID="roomPlace2" runat="server" CssClass="roomSwatch" />
+    <asp:Image ID="roomPlace3" runat="server" CssClass="roomSwatch" />
+    <asp:Image ID="ottomanPlace1" runat="server" CssClass="ottomanSwatch" />
+    <asp:Image ID="ottomanPlace2" runat="server" CssClass="ottomanSwatch" />
+    <asp:Image ID="ottomanPlace3" runat="server" CssClass="ottomanSwatch" />
+
 </asp:Content>
 
--- inspiration.aspx.cs
+++ inspiration.aspx.cs
@@ -23,5 +23,163 @@ public partial class inspiration : System.Web.UI.Page
         boardName = (string)Session["boardName"];
         curUser = (user)Session["curUser"];
         usersBoardColl = myDB.getUsersBoardCollection(curUser);
+        loadColors(usersBoardColl, boardName);
+        loadChairs(usersBoardColl, boardName);
+        loadRugs(usersBoardColl, boardName);
+        loadPatterns(usersBoardColl, boardName);
+        loadRooms(usersBoardColl, boardName);
+        loadOttomans(usersBoardColl, boardName);
     }
+    
+    public void loadColors(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> colors = coll.Find(brd => brd.Type == "Color" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item color in colors)
+        {
+            if (i == 1)
+            {
+                colorPlace1.ImageUrl = color.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                colorPlace2.ImageUrl = color.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                colorPlace3.ImageUrl = color.Image_Link;
+            }
+        }
+    }
+    public void loadChairs(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> chairs = coll.Find(brd => brd.Type == "Chair" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item chair in chairs)
+        {
+            if (i == 1)
+            {
+                chairPlace1.ImageUrl = chair.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                chairPlace2.ImageUrl = chair.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                chairPlace3.ImageUrl = chair.Image_Link;
+            }
+        }
+    }
+    public void loadRugs(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> rugs = coll.Find(brd => brd.Type == "Rugs" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item rug in rugs)
+        {
+            if (i == 1)
+            {
+                rugPlace1.ImageUrl = rug.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                rugPlace2.ImageUrl = rug.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                rugPlace3.ImageUrl = rug.Image_Link;
+            }
+        }
+    }
+    public void loadPatterns(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> patterns = coll.Find(brd => brd.Type == "Pattern" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item pattern in patterns)
+        {
+            if (i == 1)
+            {
+                patternPlace1.ImageUrl = pattern.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                patternPlace2.ImageUrl = pattern.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                patternPlace3.ImageUrl = pattern.Image_Link;
+            }
+        }
+    }
+    public void loadRooms(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> rooms = coll.Find(brd => brd.Type == "LivingRoom" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item room in rooms)
+        {
+            if (i == 1)
+            {
+                roomPlace1.ImageUrl = room.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                roomPlace2.ImageUrl = room.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                roomPlace3.ImageUrl = room.Image_Link;
+            }
+        }
+    }
+    public void loadOttomans(IMongoCollection<board_item> coll, string boardName)
+    {
+        List<board_item> ottomans = coll.Find(brd => brd.Type == "Ottoman" && brd.Board_Name == boardName)
+            .ToListAsync()
+            .Result;
+
+        int i = 1;
+        foreach (board_item otm in ottomans)
+        {
+            if (i == 1)
+            {
+                ottomanPlace1.ImageUrl = otm.Image_Link;
+                i++;
+            }
+            else if (i == 2)
+            {
+                ottomanPlace2.ImageUrl = otm.Image_Link;
+                i++;
+            }
+            else if (i == 3)
+            {
+                ottomanPlace3.ImageUrl = otm.Image_Link;
+            }
+        }
+    }
+    
 }
